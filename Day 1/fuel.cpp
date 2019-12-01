e8c75a8d6914eb82dfa62da89d54cf30e785afc1
Fergus Molloy <fdm504@york.ac.uk>
2019-12-01 09:33:33 +0000

    Completed day 1

---
+++ Day 1/fuel.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cmath>
+#include <fstream>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+int fuel(int mass){
+    double fuel_amount;
+    fuel_amount = mass/3.0;
+    fuel_amount = floor(fuel_amount) - 2;
+    if(fuel_amount<=0)
+        return 0;
+    return (int) (fuel_amount + fuel(fuel_amount));
+}
+
+int main(){
+    vector<string> lines;
+    ifstream infile("input.txt");
+    string line;
+    int sum=0;
+    int temp;
+    while(getline(infile, line)){
+        cout << "mass: " << line << endl;
+        stringstream value(line);
+        value >> temp;
+        temp = fuel(temp);
+        cout << "fuel: " << temp << endl;
+        sum += temp;
+    }
+    cout << "total fuel: " << sum << endl;
+    
+    return 0;
+}
