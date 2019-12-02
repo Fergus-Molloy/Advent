c38ec31957ca607aa24d1c29d81d5a7febe4552e
Fergus Molloy <fdm504@york.ac.uk>
2019-12-02 17:06:53 +0000

    bruh why isn't this working

--- Day 2/computer.cpp
+++ Day 2/computer.cpp
@@ -10,44 +10,30 @@ using namespace std;
 namespace program{
     vector<string> memory;
     enum INSTRUCTIONS{
-        ADD = 1, // instruction set starts at 1
+        ADD      = 1, // instruction set starts at 1
         MULTIPLY = 2,
-        HALT=99
+        HALT     = 99
+    };
+    enum IOSTREAM{
+        OUTPUT_ADDR = 0,
+        NOUN_ADDR   = 1,
+        VERB_ADDR   = 2
     };
 }
 
 
 int main(){
-    string data = readall("input.txt");
-    program::memory = split(data, ',');
-    for(int i=0; i<program::memory.size(); i+=4){
-        int  inst, var1, var2, addr;
-
-        inst = str_to_int(program::memory[i]);   // instruction
-        var1 = str_to_int(program::memory[i+1]); // address of value 1
-        var2 = str_to_int(program::memory[i+2]); // address of value 2
-        addr = str_to_int(program::memory[i+3]); // address to store at e.g. 0
-        
-        int value1, value2;
-        value1 = str_to_int(program::memory[var1]);
-        value2 = str_to_int(program::memory[var2]);
-        
-        #ifdef DEBUG
-        cout << "Instruction:\n" 
-        << program::memory[i]   << ","
-        << program::memory[i+1] << ","
-        << program::memory[i+2] << ","
-        << program::memory[i+3] << endl;
-        cout << "Decoded Instruction:\n" 
-        << str_to_int(program::memory[i])   << ","
-        << value1 << ","
-        << value2 << ","
-        << str_to_int(program::memory[addr]) << endl;
-        #endif
-
-        program::memory[addr] = to_string(compute(inst, value1, value2));
+    int output;
+    for(int i=0; i<100; i++){
+        for(int j=0; j<100; j++){
+            run_code_file("input.txt", i, j);
+            output = str_to_int(program::memory[program::OUTPUT_ADDR]);
+            if(output==RESULT){
+                cout << "i: " << i << " j: " << j << endl;
+                exit(0);
+            }
+        }
     }
-    finish();
     return 0;
 }
 
@@ -80,8 +66,8 @@ string readall(string filename){
 }
 
 void finish(){
-    cout << "Output: " << str_to_int(program::memory[OUTPUT_ADDR]) << endl;
-    exit(0);
+    cout << "Output: " << str_to_int(program::memory[program::OUTPUT_ADDR]) << endl;
+    return;
 }
 
 int compute(int instruction, int value1, int value2){
@@ -92,8 +78,65 @@ int compute(int instruction, int value1, int value2){
             return value1 * value2;
         case(program::HALT):
             finish();
+            return -1;
         default:
-            cerr << "Something went wrong" << endl;
+            cerr << "Something went wrong instruction read: " << instruction << endl;
             exit(1);
+    }
+}
+
+void print_instruction(int addr){
+        cout << "Instruction:\n" 
+        << program::memory[addr]   << ","
+        << program::memory[addr+1] << ","
+        << program::memory[addr+2] << ","
+        << program::memory[addr+3] << endl;
+}
+
+void print_decoded_instuction(int inst, int value1, int value2, int addr){
+    cout << "Decoded Instruction:\n" 
+        << inst   << ","
+        << value1 << ","
+        << value2 << ","
+        << str_to_int(program::memory[addr]) << endl;
+}
+
+void run_code_file(string filename){
+    string data = readall(filename);
+    program::memory = split(data, ',');
+    main_loop();
+}
+
+void run_code_file(string filename, int noun, int verb){
+    string data = readall(filename);
+    program::memory = split(data, ',');
+    program::memory[program::NOUN_ADDR] = noun;
+    program::memory[program::VERB_ADDR] = verb;
+    main_loop();
+    
+}
+
+void main_loop(){
+    int result;
+    for(int i=0; i<program::memory.size(); i+=4){
+        int  inst, var1, var2, addr;
+        inst = str_to_int(program::memory[i]);   // instruction
+        var1 = str_to_int(program::memory[i+1]); // address of value 1
+        var2 = str_to_int(program::memory[i+2]); // address of value 2
+        addr = str_to_int(program::memory[i+3]); // address to store at e.g. 0
+        
+        int value1, value2;
+        value1 = str_to_int(program::memory[var1]);
+        value2 = str_to_int(program::memory[var2]);
+        
+        #ifdef DEBUG
+        print_instruction(i);
+        print_decoded_instruction(inst, value1, value2, addr)        
+        #endif
+        result = compute(inst, value1, value2);
+        if(result != -1)
+            program::memory[addr] = to_string(result);
+        else
+            break;
     }
 }
--- Day 2/computer.h
+++ Day 2/computer.h
@@ -1,13 +1,18 @@
 #ifndef COMPUTER_D
 #define COMPUTER_D
 #define RESULT 19690720
-#define OUTPUT_ADDR 0
 
 int str_to_int(std::string text);
 
 int compute(int instruction, int value1, int value2);
 
 void finish();
+
+void run_code_file(std::string filename);
+
+void run_code_file(std::string filename, int noun, int verb);
+
+void main_loop();
 
 std::string readall(std::string filename);
 
